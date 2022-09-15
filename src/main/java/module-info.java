8994b3ae829d4a921eb7e73e2aed2220f7bacd0e
khumalo611 <s224580434@mandela.ac.za>
2022-09-15 15:27:08 +0200

    Progress Check Session

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.example.design_3 {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.controlsfx.controls;
+   // requires org.controlsfx.controls;
     requires java.sql;
 
     opens com.example.design_3 to javafx.fxml;
